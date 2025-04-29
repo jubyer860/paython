2d5109578a7acc61195c80c75462d4e20d5e8d20
jubyer <jubyerjhe@gmail.com>
2025-04-29 12:26:56 +0600

    intial commit

---
+++ function.py
@@ -0,0 +1,7 @@
+str = "abdullh"
+print (str[-4:-6])
+print (str.endswith("llh"))
+print (str.capitalize())
+print (str.replace("l","f"))
+print (str.find("u"))
+print (str.count("l")) 
---
+++ index.py
@@ -0,0 +1,5 @@
+str = "Abdullah Al Jubyer"
+print (str[1:8])
+print (str[:8])
+print (str[8:])
+print (str[-4:-6])
