744e878ea7d446d03a8a3208dcbbc58702db452c
jubyer <jubyerjhe@gmail.com>
2025-04-30 10:48:10 +0600

    initial commit

---
+++ oddeven.py
@@ -0,0 +1,6 @@
+num = int (input(" inter the int number : "))
+rem = num % 2
+if(num % 2 == 0):
+    print("even")
+else:
+    print("odd")
---
+++ test2.py
@@ -0,0 +1,4 @@
+#name = input("first name : ")
+#print("length of name is",len(name))
+str = (" jubyer $  hyuu $ $ abdullah$")
+print(str.count("$"))
