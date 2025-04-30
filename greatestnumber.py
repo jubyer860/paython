52158dd10c7c2689bff5bd8442548a3a78c09253
jubyer <jubyerjhe@gmail.com>
2025-04-30 11:02:10 +0600

    initial commit

---
+++ greatestnumber.py
@@ -0,0 +1,9 @@
+a = int (input("enter the number : "))
+b = int (input("enter the number : "))
+c = int (input("enter the number : "))
+if (a >= b and a >= c ):
+    print("first number is largest",a)
+elif (b>c):
+    print("2nd number is largest",b)
+else:
+    print("3rd number is largest",c)
